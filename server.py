53b8ee58654a69aaaa3d843266541cc14a107b3c
JC <bean3den@gmail.com>
2022-07-08 10:41:12 +0200

    Email error solved
    added a try/ except(IndexError) in showSummary function

--- server.py
+++ server.py
@@ -26,8 +26,11 @@ def index():
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    try:
+        club = [club for club in clubs if club['email'] == request.form['email']][0]
+    except IndexError:
+        return index(error_message="Sorry, that email wasn't found.")
+    return render_template('welcome.html', club=club, competitions=competitions)
 
 
 @app.route('/book/<competition>/<club>')
