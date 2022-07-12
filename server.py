139997bf02b82cb62d0e057ce804cdcf09405332
JC <bean3den@gmail.com>
2022-07-12 13:22:22 +0200

    Added tests
    - date_str_split
    - date_time_check
    - showSummary(check if "book places" link is in place)
    
    Added test database
    
    Added app.run in server.py

--- server.py
+++ server.py
@@ -1,16 +1,17 @@
 import json
 import datetime
-from flask import Flask,render_template,request,redirect,flash,url_for
+from flask import Flask, render_template, request, redirect, flash, url_for
+import os
 
 
 def loadClubs():
-    with open('clubs.json') as c:
+    with open(os.getcwd()+'/database/clubs.json') as c:
          listOfClubs = json.load(c)['clubs']
          return listOfClubs
 
 
 def loadCompetitions():
-    with open('competitions.json') as comps:
+    with open(os.getcwd()+'/database/competitions.json') as comps:
          listOfCompetitions = json.load(comps)['competitions']
          return listOfCompetitions
 
@@ -20,6 +21,7 @@ app.secret_key = 'something_special'
 
 competitions = loadCompetitions()
 clubs = loadClubs()
+
 
 def datetime_check(competition):
     today = date_str_split(str(datetime.datetime.now()))
@@ -33,8 +35,8 @@ def datetime_check(competition):
 
 
 def date_str_split(date):
-    days = date[:10].replace("-","")
-    hours = date[11:16].replace(":","")
+    days = date[:10].replace("-", "")
+    hours = date[11:16].replace(":", "")
     date = days+hours
     return str(date)
 
@@ -43,16 +45,17 @@ def date_str_split(date):
 def index():
     return render_template('index.html')
 
-@app.route('/showSummary',methods=['POST'])
+
+@app.route('/showSummary', methods=['POST'])
 def showSummary():
     club = [club for club in clubs if club['email'] == request.form['email']][0]
     for i in competitions:
         i = datetime_check(i)
-    return render_template('welcome.html',club=club,competitions=competitions)
+    return render_template('welcome.html', club=club, competitions=competitions)
 
 
 @app.route('/book/<competition>/<club>')
-def book(competition,club):
+def book(competition, club):
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
     if foundClub and foundCompetition:
@@ -68,7 +71,7 @@ def purchasePlaces():
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    with open('competitions.json', "w") as cr:
+    with open('database/competitions.json', "w") as cr:
         data = {'competitions': competitions}
         json.dump(data, cr)
     flash('Great-booking complete!')
@@ -81,3 +84,7 @@ def purchasePlaces():
 @app.route('/logout')
 def logout():
     return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
